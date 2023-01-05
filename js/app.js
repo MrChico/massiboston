d6e8c01f6b8b3ca020ca055cf4e5f2145e835727
MassiCerioni <61080153+MassiCerioni@users.noreply.github.com>
2023-01-05 21:29:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let rms2;
 let device;
 
 async function setupRNBO() {
-    const patchExportURL = "export/2 Species Interaction Synth.export.json";
+    const patchExportURL = "export/patcher.export.json";
 
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
