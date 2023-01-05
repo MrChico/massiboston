efeea7e8d414299f948d70ffbc091f9977b38848
MassiCerioni <61080153+MassiCerioni@users.noreply.github.com>
2023-01-05 23:26:52 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -96,8 +96,8 @@ async function setupRNBO() {
   
 
     // (Optional) Extract the name and rnbo version of the patcher from the description
-    document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
-
+//  document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
+    document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher");
     // (Optional) Automatically create sliders for the device parameters
     makeSliders(device);
 
